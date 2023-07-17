9e9ad3091fbd334889af23129bd102a08d111f79
secretoover <109528541+suzxixn@users.noreply.github.com>
2023-07-17 14:40:42 +0900

    Add files via upload

---
+++ 클래스 변수.py
@@ -0,0 +1,16 @@
+class User:
+    count = 0
+
+    def __init__(self, name, email, pw):
+        self.name = name
+        self.email = email
+        self.pw = pw
+
+        User.count += 1
+
+user1 = User("강영훈", "younghoon@codeit.kr", "123456")
+user2 = User("이윤수", "yoonsoo@codeit.kr", "abcdef")
+user3 = User("서혜린", "lisa@codeit.kr", "123abc")
+
+print(User.count)
+
